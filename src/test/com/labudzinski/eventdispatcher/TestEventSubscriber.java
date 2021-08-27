c0aa0a89f8e9e33206d76065717fb1d0a730db26
Dominik Labudzinski <dominik.labudzinski@brandoriented.pl>
2021-08-27 10:50:46 +0200

    copyright update

--- src/test/com/labudzinski/eventdispatcher/TestEventSubscriber.java
+++ src/test/com/labudzinski/eventdispatcher/TestEventSubscriber.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2021-2021. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
+ * Copyright (c) 2021-2021.
+ *
+ * This file is part of the com.labudzinski package.
+ *  For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ *  @author Dominik Labudzinski <dominik@labudzinski.com>
  */
 
 package com.labudzinski.eventdispatcher;
